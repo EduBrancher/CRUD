eaefffdc0282bee99a17cfbabae01beaa6679ae9
EduBrancher <edu.urenha93@gmail.com>
2022-05-26 11:57:37 -0300

    commit inicial

---
+++ src/main/java/com/urenha/ddsheet/DdCharacterSheetApplication.java
@@ -0,0 +1,13 @@
+package com.urenha.ddsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdCharacterSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdCharacterSheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urenha/ddsheet/DdCharacterSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urenha.ddsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdCharacterSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
