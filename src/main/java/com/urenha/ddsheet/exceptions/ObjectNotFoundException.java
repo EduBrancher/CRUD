5ef09bc79ef72aaa7bb329a861d9348cf3d7a048
EduBrancher <edu.urenha93@gmail.com>
2022-05-27 17:59:07 -0300

    Exceção para o objeto não encontrado

---
+++ src/main/java/com/urenha/ddsheet/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.urenha.ddsheet.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    private static final long serialVersionUID = 1l;
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/urenha/ddsheet/services/CharacterCategoryService.java
+++ src/main/java/com/urenha/ddsheet/services/CharacterCategoryService.java
@@ -1,6 +1,7 @@
 package com.urenha.ddsheet.services;
 
 import com.urenha.ddsheet.CharacterCategory;
+import com.urenha.ddsheet.exceptions.ObjectNotFoundException;
 import com.urenha.ddsheet.repositories.CharacterCategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ public class CharacterCategoryService {
 
     public CharacterCategory findById(Integer id){
         Optional<CharacterCategory> obj = categoryRepo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado: " + id + "Tipo: " + CharacterCategory.class.getName()));
     }
 }
